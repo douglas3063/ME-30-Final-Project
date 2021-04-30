95940d9d53f7d55c532c0375b2409ff7b068f5c0
Elizabeth S. B <esbremberg@gmail.com>
2021-04-30 14:37:12 -0700

    adding the questions that jason wrote
    just adding the questions that jason wrote. and futzing around with virtual coding studio

--- ME-30-Final-Project-main/trivia_q_a.py
+++ ME-30-Final-Project-main/trivia_q_a.py
@@ -4,11 +4,29 @@ Objects could have attributes of category, question, answer, hint, etc.
 """
 
 trivia_dictionary = {
-    "What is the capital of California?":"Sacramento",
-    "What year was SJSU founded":"1857",
-    "What is the element 'Fe'":"Iron",
-    "What is the element 'Au'":"Gold",
-    "What is the element 'Ag'":"Silver",
-    "What is the element 'He'":"Helium",
-    "What is the element 'Cu'":"Copper"
+    "What is the capital of California?": "Sacramento",
+    "How many continents are there?": "7",
+    "What is the smallest country?": "Vatican City",
+    "What is the longest river in the world?": "Nile",
+    "What country has the longest total coastline?": "Canada",
+    "What year was SJSU founded": "1857",
+    "What year was the Declaration of Independence signed?": "1776",
+    "What year did humans first land on the Moon?": "1969",
+    "What year did the US acquire the Louisiana Purchase?": "1803",
+    "What year was the Magna Carta signed?": "1215",
+    "What is the element 'Fe'": "Iron",
+    "What is the element 'Au'": "Gold",
+    "What is the element 'Ag'": "Silver",
+    "What is the element 'He'": "Helium",
+    "What is the element 'Cu'": "Copper",
+    "What is the largest organ in the human body?": "Skin",
+    "What is the longest bone in the human body?": "Femur",
+    "Which grow faster fingernails or toenails?": "Fingernails",
+    "What organ produces insulin?": "Pancreas",
+    "How many blood types are there?": "8",
+    "Who invented basketball?": "James Naismith",
+    "Who invented Monopoly?": "Elizabeth Magie",
+    "Who invented the windshield wiper?": "Mary Anderson",
+    "Who invented Hot Cheetos?": "Richard Montanez",
+    "Who invented the ice cream maker?": "Nancy Johnson"
 }
