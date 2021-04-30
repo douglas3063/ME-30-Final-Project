29141beca3d3d9bba192c2597fef4e588c58e605
Elizabeth S. B <esbremberg@gmail.com>
2021-04-30 16:10:49 -0700

    creating foundations for adding points to existing points
    based on the replacement function we had in class

--- ME-30-Final-Project-main/Participant.py
+++ ME-30-Final-Project-main/Participant.py
@@ -6,9 +6,10 @@ class Participant:
         self.user = message.author # discord api
         self.points = points
 
-    def add_points(self):
+    def add_points(self,newPoints):
         # based on correct answer from trivia response
-        pass
+        #newPoints = (however you get the data from the other file)
+        return self.points += self.newPoints
     
     def __str__(self):
         return "{} has {} points".format(self.user, self.points)
